691c5bcd2444ee8f10bb674b21ed90a786b900a8
Zac Steinberg <zstein1234@gmail.com>
2019-08-21 21:57:26 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -25,7 +25,9 @@ def end_game(card_total)
 end
 
 def initial_round
-  display_card_total(deal_card + deal_card)
+  total = deal_card + deal_card
+  display_card_total(total)
+  total
 end
 
 def hit?
