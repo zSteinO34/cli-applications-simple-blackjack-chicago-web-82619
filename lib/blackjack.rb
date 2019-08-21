6cc772f155198d94b31c06ac9fe45344def64bb7
Zac Steinberg <zstein1234@gmail.com>
2019-08-21 21:35:35 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -18,8 +18,8 @@ def get_user_input
   gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
