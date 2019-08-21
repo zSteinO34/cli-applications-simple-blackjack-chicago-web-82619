22b01d1df057a654762010f18126829441761bbc
Zac Steinberg <zstein1234@gmail.com>
2019-08-21 22:21:23 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -38,7 +38,7 @@ def hit?(card_total)
   elsif choice == 'h'
     new_total = card_total += deal_card
     new_total
-  else
+  elsif choice !== 'h' || choice !== 's'
     invalid_command
     prompt_user
   end
